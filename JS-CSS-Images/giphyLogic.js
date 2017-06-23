1d632f58b5a7b1f2088ef42073a862d455de6617
Josh Wood <wood2jp2@gmail.com>
2017-06-23 18:47:59 -0400

    ten gifs load and now can be frozen and unfrozen. need to get input form working

--- JS-CSS-Images/giphyLogic.js
+++ JS-CSS-Images/giphyLogic.js
@@ -1,23 +1,56 @@
 $(document).ready(function(){
 
+		var userInput = $("#userInput").val().trim() + "dog";
+    	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=f4f070759ff64c25affb59086f863cf6&limit=10";
 
-	var userInput = $("#userInput").val().trim();
-    var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + userInput;
-	$.ajax({
-		url: queryURL,
-		method: "GET",
-	}).done(function(response) {
+    	// calling API...
+		$.ajax({
+			url: queryURL,
+			method: "GET",
+		}).done(function(response) {
 
-		var carImage = $("<img>");
-		var carButton = $("<button>");
+		var results = response.data;
+		var i = 0;
+		var picture;
 
-		console.log(response);
-		console.log(response.data.url);
-		$(carImage).attr("src", response.data.image_original_url);
-		$('#gifResults').prepend(carImage);
+		for (i; i<results.length; i++) {
+
+			var carDiv = $('<div>');
+			var carImage = $("<img>");
+			var carButton = $("<button>");
+			picture = results[i].images.fixed_height_still.url;
+			var ratingP = "Rating is: " + results[i].rating;
+
+			console.log(response);
+			// adding all attributes necessary to the image before appending to page
+			carImage.attr("src", picture);
+			carImage.attr("alt", "car-gif");
+			carImage.attr("data-state", "still");
+			carImage.attr("data-still", picture);
+			carImage.attr("data-animate", results[i].images.fixed_height.url);
+			carImage.addClass("gif");
+
+			// appending the rating and the picture together before appending to page
+			carDiv.prepend(carImage, ratingP);
+			$('#gifResults').prepend(carDiv);	
+		}
+
 		carButton.text(userInput);
+
 		$('#gifButtons').prepend(carButton);
 
+		$(".gif").on("click", function() {
 
-});
-});
+			var state = $(this).attr("data-state");
+
+			if (state==='still') {
+				$(this).attr("src", $(this).attr("data-animate"));
+				$(this).attr("data-state", "animate");
+			} else {
+				$(this).attr("src", $(this).attr("data-still"));
+				$(this).attr("data-state", "still")
+			}
+		});
+
+		});
+	});
