699968ec1b300bbb9296071b806c4d2a42fcb302
Josh Wood <wood2jp2@gmail.com>
2017-06-23 23:38:01 -0400

    threw most of this into an object...now gif freezing wont work...stand by

--- JS-CSS-Images/giphyLogic.js
+++ JS-CSS-Images/giphyLogic.js
@@ -1,83 +1,112 @@
 $(document).ready(function(){
 
-    	var topics = ["cars", "dogs", "pizza", "seinfeld", "GoT", "baseball", "coding", "future", "robots"];
-    	var i = 0;
+	// geting my Object-Oriented Programming on
+	var giphy = {
+
+    	topics: ["cars", "dogs", "pizza", "seinfeld", "GoT", "baseball", "coding", "future", "robots"],
+    	i: 0,
 
     	// populating buttons for pre-existing array topics
-    	for (i=0; i<topics.length; i++) {
-    		var populateButton = $('<button>');
-    		populateButton.text(topics[i]);
-    		populateButton.attr("id", topics[i]);
-    		populateButton.addClass("button");
-    		$('#gifButtons').append(populateButton);
-    	}
+    	populateButtons: function() {
+    		for (i=0; i<this.topics.length; i++) {
+    			var populateButton = $('<button>');
+    			populateButton.text(this.topics[i]);
+    			populateButton.attr("id", this.topics[i]);
+    			populateButton.addClass("button");
+    			$('#gifButtons').append(populateButton);
+    		}
+    	},	
 
-    	$('#addButton').on("click", function(event) {
-    		event.preventDefault();
-    		var userInput = $("#userInput").val().trim();
-    		var addNewButton = $('<button>');
-    		addNewButton.text(userInput);
-    		addNewButton.attr("id", userInput);
-    		addNewButton.addClass("button");
-    		$('#gifButtons').append(addNewButton);
-    		$('#userInput').clear;
-    	})
+    	addButton: function() {
+    		$('#addButton').on("click", function(event) {
+    			event.preventDefault();
+    			var userInput = $("#userInput").val().trim();
+    			var addNewButton = $('<button>');
+    			addNewButton.text(userInput);
+    			addNewButton.attr("id", userInput);
+    			addNewButton.addClass("button");
+    			$('#gifButtons').append(addNewButton);
+    			$('#userInput').text("");
+    		});
+    	},
 
-    	$('.button').on('click', function() {
-    		var currentButtonId = $(this).attr("id");
-    		callAPI();
+    	displayGifs: function() {
+    		$('.button').on('click', function() {
+    			currentButtonId = $(this).attr("id");
+    			callAPI();
     	
+		    	function callAPI() {
+		    		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + currentButtonId + "&api_key=f4f070759ff64c25affb59086f863cf6&limit=10";
 
-    	function callAPI() {
-    	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + currentButtonId + "&api_key=f4f070759ff64c25affb59086f863cf6&limit=10";
+		    		// calling API...
+					$.ajax({
+						url: queryURL,
+						method: "GET",
+					}).done(function(response) {
 
-    	// calling API...
-		$.ajax({
-			url: queryURL,
-			method: "GET",
-		}).done(function(response) {
+					var results = response.data;
+					var i = 0;
+					var picture;
 
-		var results = response.data;
-		var i = 0;
-		var picture;
+					for (i; i<results.length; i++) {
 
-		for (i; i<results.length; i++) {
+						var carDiv = $('<div>');
+						var carImage = $("<img>");
+						var carButton = $("<button>");
+						picture = results[i].images.fixed_height_still.url;
+						var ratingP = "Rating is: " + results[i].rating;
 
-			var carDiv = $('<div>');
-			var carImage = $("<img>");
-			var carButton = $("<button>");
-			picture = results[i].images.fixed_height_still.url;
-			var ratingP = "Rating is: " + results[i].rating;
+						console.log(response);
 
-			console.log(response);
+						// adding all attributes necessary to the image before appending to page
+						carImage.attr("src", picture);
+						carImage.attr("alt", "car-gif");
+						carImage.attr("data-state", "still");
+						carImage.attr("data-still", picture);
+						carImage.attr("data-animate", results[i].images.fixed_height.url);
+						carImage.addClass("gif");
 
-			// adding all attributes necessary to the image before appending to page
-			carImage.attr("src", picture);
-			carImage.attr("alt", "car-gif");
-			carImage.attr("data-state", "still");
-			carImage.attr("data-still", picture);
-			carImage.attr("data-animate", results[i].images.fixed_height.url);
-			carImage.addClass("gif");
+						// appending the rating and the picture together before appending to page
+						carDiv.prepend(carImage, ratingP);
+						$('#gifResults').prepend(carDiv);	
+						};
+					});
+				};
+			});
+		},
 
-			// appending the rating and the picture together before appending to page
-			carDiv.prepend(carImage, ratingP);
-			$('#gifResults').prepend(carDiv);	
-		}
+		// pauseGifs: function() {
+		// 	console.log(this);
+		// 	$(".gif").on("click", function() {
+		// 		console.log("poop again");
+		// 		var state = $(this).attr("data-state");
 
-		$(".gif").on("click", function() {
+		// 		if (state==='still') {
+		// 			$(this).attr("src", $(".gif").attr("data-animate"));
+		// 			$(this).attr("data-state", "animate");
+		// 		} else {
+		// 			$(this).attr("src", $(".gif").attr("data-still"));
+		// 			$(this).attr("data-state", "still");
+		// 		};
+		// 	});
+		// },
+	};
 
-			var state = $(this).attr("data-state");
+				$(".gif").on("click", function() {
+				console.log("poop again");
+				var state = $(this).attr("data-state");
 
-			if (state==='still') {
-				$(this).attr("src", $(this).attr("data-animate"));
-				$(this).attr("data-state", "animate");
-			} else {
-				$(this).attr("src", $(this).attr("data-still"));
-				$(this).attr("data-state", "still")
-			}
-		});
+				if (state==='still') {
+					$(this).attr("src", $(".gif").attr("data-animate"));
+					$(this).attr("data-state", "animate");
+				} else {
+					$(this).attr("src", $(".gif").attr("data-still"));
+					$(this).attr("data-state", "still");
+				};
+			});
+		giphy.populateButtons();
+		giphy.addButton();
+		giphy.displayGifs();
+		// giphy.pauseGifs();
 
-	});	
-};
-});
 	});
